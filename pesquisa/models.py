ed26eafbdfd97833f0cd2cb7997a3f9878ca2445
Suelen Pessoa <suelenoliveira2111@gmail.com>
2019-02-26 21:59:34 -0300

    Alterações na Model

--- pesquisa/models.py
+++ pesquisa/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pergunta(models.Model): 
+    texto = models.CharField()
+    data = models.DateField()
+
+class Alternativa(models.Model):
+    texto = models.CharField()
+    votos = models.IntegerField()
+       
