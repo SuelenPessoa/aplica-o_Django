c0fc36015da5f1525690cbf3a08a5b03a6b39262
Suelen Pessoa <suelenoliveira2111@gmail.com>
2019-02-27 21:50:23 -0300

    Mostrar enquete no index.html

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from . models import Pergunta
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    todas = Pergunta.objects.all()  #retorna todas as perguntas cadastradas
+    return render(request, 'index.html',{
+        "perguntas": todas #volta o valor da variavel todas que foi passado acima , dicionario.
+
+    })
